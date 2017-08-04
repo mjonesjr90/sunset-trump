6eea40b306939073c6eaa7f390532b3762f47dff
malcom.jones <malcom.jones@cnu.edu>
2017-08-04 00:42:43 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,19 +1,47 @@
 //Unix epoch start time (January 1, 1970)
 //Curent time in seconds since Unix start
-var cts = new Date().getTime() / 1000;
+var cts = Math.round(new Date().getTime() / 1000);
+console.log("ct: " + cts);
+
+//Beginning of the First Term
+var bft = new Date(2017,0,21,00,00,00,0);
+var bfts = Math.round(bft.getTime() / 1000);
+console.log("bt: " + bfts);
 
 //End of the First Term
 var eft = new Date(2021,0,21,00,00,00,0);
-var efts = eft.getTime() / 1000;
+var efts = Math.round(eft.getTime() / 1000);
 
 //End of the Second Term
 var est = new Date(2025,0,21,00,00,00,0);
-var ests = eft.getTime() / 1000;
+var ests = Math.round(est.getTime() / 1000);
+
+//Length of Term in Seconds
+var termLength = efts - bfts;
+console.log("term length: " + termLength);
+
+//Time Passed
+var timePassed = cts - bfts;
+console.log("time passed: " + timePassed);
+
+//Time Remaining
+var timeRemaining = efts - cts;
+console.log("time remaining:" + timeRemaining);
 
 //Normalize values based on screen size
-
+// screen width = length of term
+// ------------   --------------
+//  start         point in term
+//
+// current loc = (screen width * point in term) / length of term
+// screen width = (length)
 var ww = window.innerWidth;
 var hh = window.innerHeight;
+var start = (ww * timePassed) / termLength;
+console.log("current width: " + start);
+
+var change = false;
+console.log("change: " + change);
 
 $(window).resize(function() {
    ww = window.innerWidth;
@@ -24,18 +52,28 @@ $(window).resize(function() {
 });
 
 $(window).load(function() {
+   $("#logo").parent().css({position: 'relative'});
+   $("#logo").css({left: start, position:'absolute'});
    $("#demo").append("<div id=\"st\">Starting width is:" + ww + " </div>");
-   $("#demo").append("<div id=\"eft\">First Term End Date:" + eft + " </div>");
-   $("#demo").append("<div id=\"efts\">First Term End Time:" + efts + " </div>");
-   $("#demo").append("<div id=\"est\">Second Term End Date:" + est + " </div>");
-   $("#demo").append("<div id=\"ests\">Second Term End Time:" + ests + " </div>");
+   $("#demo").append("<div id=\"lt\">Length of Term:" + termLength + " </div>");
    var logo = document.getElementById("logo");
-   TweenLite.to(logo, 5, {left:"632px"});
+   TweenLite.to(logo, timeRemaining, {left:ww});
+});
+
+$("#now").click(function() {
+   change = true;
+   console.log("change: " + change);
+   var logo1 = document.getElementById("logo");
+   TweenLite.to(logo1, 5, {left:ww});
 });
 
 //Set to occur every second
 setInterval(function(){
-   cts = new Date().getTime() / 1000;
+   cts = Math.round(new Date().getTime() / 1000);
    var rt = efts - cts;
    $("#rt").replaceWith("<div id=\"rt\">Time until leaving office:" + rt + " </div>");
+   if(change){
+      var logo1 = document.getElementById("logo");
+      TweenLite.to(logo1, 5, {left:ww});
+   }
 }, 1000);
